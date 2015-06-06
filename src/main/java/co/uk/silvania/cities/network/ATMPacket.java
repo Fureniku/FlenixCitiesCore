06e5e386665207c9c84ae3495bc466a50d111277
Flenix <flenix@silvania.co.uk>
2015-06-07 00:01:21 +0100

    Well, that was a lot easier than expected...

--- src/main/java/co/uk/silvania/cities/network/ATMPacket.java
+++ src/main/java/co/uk/silvania/cities/network/ATMPacket.java
@@ -1,6 +1,8 @@
 package co.uk.silvania.cities.network;
 
+import co.uk.silvania.cities.core.CoreItems;
 import io.netty.buffer.ByteBuf;
+import net.minecraft.item.ItemStack;
 import cpw.mods.fml.common.network.ByteBufUtils;
 import cpw.mods.fml.common.network.simpleimpl.IMessage;
 import cpw.mods.fml.common.network.simpleimpl.IMessageHandler;
@@ -31,6 +33,7 @@ public class ATMPacket implements IMessage {
 
 		@Override
 		public IMessage onMessage(ATMPacket message, MessageContext ctx) {
+			ctx.getServerHandler().playerEntity.inventory.addItemStackToInventory(new ItemStack(CoreItems.note1000));
 			System.out.println(String.format("Received %s from %s", message.withdrawAmount, ctx.getServerHandler().playerEntity.getDisplayName()));
 			return null;
 		}
